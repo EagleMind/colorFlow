223d231e6aa5d569fb74c317dc953d54f600953f
EagleMind <hassen.ben.mbarek@gmail.com>
2024-01-03 10:05:39 +0100

    added live preview area

--- src/App.tsx
+++ src/App.tsx
@@ -50,10 +50,12 @@ const App: React.FC = () => {
   const handleSelectBaseColorOne = (data: string) => {
 
     setBaseColorOne(data);
-
+    console.log("color 1", baseColorOne)
   };
   const handleSelectBaseColorTwo = (data: string) => {
     setBaseColorTwo(data);
+    console.log("color 2", baseColorTwo)
+
   };
   const handleGenSingleColor = (data: boolean) => {
     setGenerateSingleColor(data);
@@ -83,8 +85,12 @@ const App: React.FC = () => {
         random: generateSingleColor,
         direction: getGradDirection,
       });
-      console.log("final output")
+      console.log("final output - colors", {
+        baseColorOne: baseColorOne,
+        baseColorTwo: baseColorTwo,
+      })
       setData(colors);
+      console.log("data", data)
     }
   };
   useEffect(() => {
@@ -92,27 +98,38 @@ const App: React.FC = () => {
   }, [getCount, baseColorOne, baseColorTwo, getLerp, getHue, getLightness, getSaturatiion, getGradDirection, generateSingleColor]);
   return (
     <div className='md:min-h-screen h-full  lg:p-10'>
-      <div className="rounded-lg border shadow-sm p-4" data-v0-t="card">
+      <div className='flex flex-row w-full'>
 
-        <div className="flex p-6">
-          <div className='flex-col bg-gray-100 p-5 border rounded-md'>
-            <h3 className="text-lg font-semibold">Choose Generator</h3>
+        <div className='flex flex-col w-2/3'>
 
-            <GeneratorSelector onGeneratorSelect={handleSelectGenerator} />
+          <div className="rounded-lg border shadow-sm p-4 " data-v0-t="card">
+
+            <div className="flex  p-6">
+              <div className='flex-col bg-gray-100 p-5 border rounded-md'>
+                <h3 className="text-lg font-semibold">Choose Generator</h3>
+
+                <GeneratorSelector onGeneratorSelect={handleSelectGenerator} />
+              </div>
+
+
+              <GeneratorOptions baseColorOne={handleSelectBaseColorOne} baseColorTwo={handleSelectBaseColorTwo} selectedGenerator={selectedGenerator} handleCount={handleCount} handleLerp={handleLerp} handleHue={handleHue} handleLightness={handleLightness} handleSaturation={handleSaturation} handleGenSingleColor={handleGenSingleColor} handleGradDirection={handleGradDirection}  ></GeneratorOptions>
+
+            </div>
           </div>
+          <div className='flex  my-5'>
+            <div className='w-full rounded-lg border   bg-white lg:p-10'>
+              <h1 className='font-semibold text-3xl px-2 text-gray-500'>Generated Colors</h1>
+              {data && <ColorsView colors={data}></ColorsView>}
+            </div>
 
-
-          <GeneratorOptions baseColorOne={handleSelectBaseColorOne} baseColorTwo={handleSelectBaseColorTwo} selectedGenerator={selectedGenerator} handleCount={handleCount} handleLerp={handleLerp} handleHue={handleHue} handleLightness={handleLightness} handleSaturation={handleSaturation} handleGenSingleColor={handleGenSingleColor} handleGradDirection={handleGradDirection}  ></GeneratorOptions>
+          </div>
+        </div>
+        <div className='absolute right-0 h-full rounded-lg border shadow-sm p-4 md:w-1/3'>
+          <h2 className='text-lg font-semibold'>Live Preview</h2>
 
         </div>
       </div>
-      <div className='flex  my-5'>
-        <div className='w-full rounded-lg border   bg-white lg:p-10'>
-          <h1 className='font-semibold text-3xl px-2 text-gray-500'>Generated Colors</h1>
-          {data && <ColorsView colors={data}></ColorsView>}
-        </div>
 
-      </div>
     </div>
   );
 };
